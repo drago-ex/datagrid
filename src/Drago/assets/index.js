3ccce5b7dada011fb442d268eae8d6cb2e9140b1
Zdeněk Papučík <zdenek.papucik@gmail.com>
2026-05-12 13:00:58 +0200

    Update index.js

--- src/Drago/assets/index.js
+++ src/Drago/assets/index.js
@@ -2,8 +2,6 @@ import "./datagrid.scss";
 import DataGridFilter from "./datagrid-filter.js";
 import DataGridPage from "./page-items.js";
 
-export { DataGridFilter, DataGridPage };
-
 export default class DataGrid {
 	initialize(naja) {
 		new DataGridFilter().initialize(naja);
